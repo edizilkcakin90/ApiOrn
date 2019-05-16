7a73ef1eede62f0922d46407cce735348b0b4dbf
ediz.ilkcakin@gmail.com <ediz.ilkcakin@gmail.com>
2019-05-16 11:36:10 +0300

    Added Classes

---
+++ ApiOrnek/ApiOrnek/Data/Dummy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiOrnek.Data
+{
+    public class Dummy
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public short Age { get; set; }
+    }
+}
