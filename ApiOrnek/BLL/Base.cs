6ae5a29afcab3b5f3d469e6fc45c6822d3b74fc9
ediz.ilkcakin@gmail.com <ediz.ilkcakin@gmail.com>
2019-05-20 14:00:24 +0300

    DLL, BLL included, CRUD updated

---
+++ ApiOrnek/BLL/Base.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DLL;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Base
+    {
+        List<Dummy> dummies = Datas.Dummies.dummiesList;
+
+        public IEnumerable<Dummy> GetAll()
+        {
+            return dummies;
+        }
+
+        public Dummy GetByID(int id)
+        {
+            var dummy = dummies.FirstOrDefault(x => x.ID == id);
+            return dummy;
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var dummy = dummies.FirstOrDefault(x => x.ID == model.ID);
+                dummies.Remove(dummy);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Add(Dummy model)
+        {
+            try
+            {
+                var newDummy = new Dummy();
+                newDummy.ID = model.ID;
+                newDummy.Name = model.Name;
+                newDummy.LastName = model.LastName;
+                newDummy.Age = model.Age;
+                dummies.Add(newDummy);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Update(Dummy model)
+        {
+            var dummy = dummies.FirstOrDefault(x => x.ID == model.ID);
+            if (dummy != null)
+            {
+                dummy.Name = model.Name;
+                dummy.LastName = model.LastName;
+                dummy.Age = model.Age;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- ApiOrnek/DLL/Dummy.cs
+++ ApiOrnek/DLL/Dummy.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+using System.Text;
 using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace ApiOrnek.Data
+namespace DLL
 {
     public class Dummy
     {
