4e496c5da49a91e7a0d5b3f9566eaf63809af429
ediz.ilkcakin@gmail.com <ediz.ilkcakin@gmail.com>
2019-07-12 17:39:50 +0300

    authorization

--- ApiOrnek/Core/User.cs
+++ ApiOrnek/Core/User.cs
@@ -18,5 +18,6 @@ namespace Core
         public string IdentityNo { get; set; }
         public char Sex { get; set; }
         public string Password { get; set; }
+        public string Token { get; set; }
     }
 }
